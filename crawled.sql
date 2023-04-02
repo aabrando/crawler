8b9deb276e5648f5913ff94acefd46f7410af6fa
aabrando <67147802+aabrando@users.noreply.github.com>
2023-04-02 19:04:27 +0700

    Database
    database dump

---
+++ crawled.sql
@@ -0,0 +1,113 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.41-0ubuntu0.18.04.1 : Database - crawled
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `agents` */
+
+DROP TABLE IF EXISTS `agents`;
+
+CREATE TABLE `agents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `host_ip` varchar(32) DEFAULT NULL,
+  `browser` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `doms` */
+
+DROP TABLE IF EXISTS `doms`;
+
+CREATE TABLE `doms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `host_id` int(11) DEFAULT NULL,
+  `part_id` int(11) DEFAULT NULL,
+  `dom2find` varchar(128) DEFAULT NULL,
+  `output` varchar(32) DEFAULT 'plaintext',
+  `strneedle` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=496 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `hosts` */
+
+DROP TABLE IF EXISTS `hosts`;
+
+CREATE TABLE `hosts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `url` varchar(128) DEFAULT NULL,
+  `index` varchar(128) DEFAULT NULL,
+  `search` varchar(128) DEFAULT NULL,
+  `suffix1` varchar(64) DEFAULT NULL,
+  `suffix2` varchar(64) DEFAULT NULL,
+  `start_page` int(4) DEFAULT '1',
+  `category` varchar(32) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `enabled` tinyint(1) DEFAULT '1',
+  `class` varchar(32) DEFAULT NULL,
+  `kinds` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `kinds` */
+
+DROP TABLE IF EXISTS `kinds`;
+
+CREATE TABLE `kinds` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `movies` */
+
+DROP TABLE IF EXISTS `movies`;
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `host_id` int(11) DEFAULT NULL,
+  `title` varchar(128) DEFAULT NULL,
+  `url` varchar(128) DEFAULT NULL,
+  `image` varchar(128) DEFAULT NULL,
+  `rating` varchar(32) DEFAULT NULL,
+  `durasi` varchar(32) DEFAULT NULL,
+  `desc` text,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Table structure for table `parts` */
+
+DROP TABLE IF EXISTS `parts`;
+
+CREATE TABLE `parts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kind_id` int(11) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `playbacks` */
+
+DROP TABLE IF EXISTS `playbacks`;
+
+CREATE TABLE `playbacks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `host_id` int(11) DEFAULT NULL,
+  `path` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
